af566b5396dc74b6111d78f29e28d563927d586e
wellsla <61843654+wellsla@users.noreply.github.com>
2025-11-03 21:37:24 -0300

    chore: temporarily disable Toaster to isolate update loop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
 import { Inter, Space_Grotesk } from "next/font/google";
 import Providers from "./providers";
 
@@ -28,7 +27,8 @@ export default function RootLayout({
       >
         <Providers>
           {children}
-          <Toaster />
+          {/* Temporarily disable global Toaster to rule out update loops */}
+          {/* <Toaster /> */}
         </Providers>
       </body>
     </html>
